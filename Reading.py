00c785bfcec7f7ff20eac651b364b31a6a4956f8
Jeffrey Reagan <jeffreyreagan02@gmail.com>
2024-03-11 00:17:16 -0400

    On branch scada_with_alarms_v01  Changes to be committed: 	new file:   Reading.py 	new file:   __pycache__/Reading.cpython-312.pyc 	modified:   routes/__pycache__/__init__.cpython-312.pyc 	modified:   routes/__pycache__/main_routes.cpython-312.pyc 	modified:   routes/__pycache__/page_2_routes.cpython-312.pyc 	modified:   routes/__pycache__/page_3_routes.cpython-312.pyc 	new file:   static/js/server_scripts.js 	modified:   templates/page_2.html 	modified:   utils/__pycache__/__init__.cpython-312.pyc 	modified:   utils/__pycache__/image_generator.cpython-312.pyc 	modified:   website_1.py

---
+++ Reading.py
@@ -0,0 +1,182 @@
+from pylogix import PLC
+import time
+# Function to read the PLC tags for vacuum and seperator pressure and return the values in a readable format
+
+print("Does this print once?")
+
+def read_plc_tag():
+        try:
+            with PLC() as comm:
+                comm.IPAddress = '172.16.21.12' 
+                comm.ProcessorSlot = 0  
+                tags = ['VACUUM_1_VACUUM','VACUUM_1_SEPARATOR_PRESSURE','VACUUM_2_VACUUM','VACUUM_2_SEPARATOR_PRESSURE','VACUUM_3_VACUUM','VACUUM_3_SEPARATOR_PRESSURE','VACUUM_4_VACUUM','VACUUM_4_SEPARATOR_PRESSURE','VACUUM_5_VACUUM','VACUUM_5_SEPARATOR_PRESSURE']
+                pump1vacuum = None
+                pump2vacuum = None
+                pump3vacuum = None
+                pump4vacuum = None
+                pump5vacuum = None
+                seperator1_psi = None
+                seperator2_psi = None
+                seperator3_psi = None
+                seperator4_psi = None
+                seperator5_psi = None
+                
+                results = comm.Read(tags)
+                for result in results:
+                    if result.Status == 'Success':
+                        if result.TagName == 'VACUUM_1_VACUUM':
+                            result.Value *= 0.1
+                            pump1vacuum_formatted = "{:.2f}".format(result.Value)
+                            pump1vacuum = str(pump1vacuum_formatted) + " Hg"
+                        if result.TagName == 'VACUUM_1_SEPARATOR_PRESSURE':
+                            result.Value *= 0.1
+                            seperator1_psi_formatted = "{:.2f}".format(result.Value)
+                            seperator1_psi = str(seperator1_psi_formatted) + " PSI"
+                        if result.TagName == 'VACUUM_2_VACUUM':
+                            result.Value *= 0.1
+                            pump2vacuum_formatted = "{:.2f}".format(result.Value)
+                            pump2vacuum = str(pump2vacuum_formatted) + " Hg"
+                        if result.TagName == 'VACUUM_2_SEPARATOR_PRESSURE':
+                            result.Value *= 0.1
+                            seperator2_psi_formatted = "{:.2f}".format(result.Value)
+                            seperator2_psi = str(seperator2_psi_formatted) + " PSI"
+                        if result.TagName == 'VACUUM_3_VACUUM':
+                            result.Value *= 0.1
+                            pump3vacuum_formatted = "{:.2f}".format(result.Value)
+                            pump3vacuum = str(pump3vacuum_formatted) + " Hg"
+                        if result.TagName == 'VACUUM_3_SEPARATOR_PRESSURE':
+                            result.Value *= 0.1
+                            seperator3_psi_formatted = "{:.2f}".format(result.Value)
+                            seperator3_psi = str(seperator3_psi_formatted) + " PSI"
+                        if result.TagName == 'VACUUM_4_VACUUM':
+                            result.Value *= 0.1
+                            pump4vacuum_formatted = "{:.2f}".format(result.Value)
+                            pump4vacuum = str(pump4vacuum_formatted) + " Hg"
+                        if result.TagName == 'VACUUM_4_SEPARATOR_PRESSURE':
+                            result.Value *= 0.1
+                            seperator4_psi_formatted = "{:.2f}".format(result.Value)
+                            seperator4_psi = str(seperator4_psi_formatted) + " PSI"
+                        if result.TagName == 'VACUUM_5_VACUUM':
+                            result.Value *= 0.1
+                            pump5vacuum_formatted = "{:.2f}".format(result.Value)
+                            pump5vacuum = str(pump5vacuum_formatted) + " Hg"
+                        if result.TagName == 'VACUUM_5_SEPARATOR_PRESSURE':
+                            result.Value *= 0.1
+                            seperator5_psi_formatted = "{:.2f}".format(result.Value)
+                            seperator5_psi = str(seperator5_psi_formatted) + " PSI"
+                        
+                return pump1vacuum, seperator1_psi, pump2vacuum, seperator2_psi, pump3vacuum, seperator3_psi, pump4vacuum, seperator4_psi, pump5vacuum, seperator5_psi
+        except Exception as e:
+                    print(f"An error occurred: {e}")
+import random
+
+pump_1_status_value = ''
+pump_2_status_value = ''
+pump_3_status_value = ''
+pump_4_status_value = ''
+pump_5_status_value = ''
+
+def randomize_alarm_status():
+    pump_1_status_value = random.choice([0, 1])
+    pump_2_status_value = random.choice([0, 1])
+    return pump_1_status_value, pump_2_status_value
+
+
+
+def read_alarm_tags_all_pumps(tag_names):
+    try:
+        # Simulate reading tag values
+        tag_values = {tag: random.choice([0, 1]) for tag in tag_names}
+        return tag_values
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+
+import random
+import time
+
+def update_circle_color():
+    circle_colors = {'pump1color': '', 'pump2color': '', 'pump3color': '', 'pump4color': '', 'pump5color': ''}  # Initialize colors
+    pump_1_status_value,  pump_2_status_value = randomize_alarm_status()
+    try:
+        time.sleep(5)
+        # Simulate reading tag values
+        
+        
+        # Update circle colors based on simulated tag values
+        if pump_1_status_value == 1:
+            circle_colors['pump1color'] = 'green'              
+        elif pump_1_status_value == 0:
+            circle_colors['pump1color'] = 'red'
+            
+        if pump_2_status_value == 1:
+            circle_colors['pump2color'] = 'green'
+        elif pump_2_status_value == 0:
+            circle_colors['pump2color'] = 'red'
+            
+        if pump_3_status_value == 1:
+            circle_colors['pump3color'] = 'green'
+        elif pump_3_status_value == 0:
+            circle_colors['pump3color'] = 'red'
+            
+        if pump_4_status_value == 1:
+            circle_colors['pump4color'] = 'green'
+        elif pump_4_status_value == 0:
+            circle_colors['pump4color'] = 'red'
+            
+        if pump_5_status_value == 1:
+            circle_colors['pump5color'] = 'green'
+        elif pump_5_status_value == 0:
+            circle_colors['pump5color'] = 'red'
+            
+        time.sleep(1)
+        print("Returning pump statuses")
+        return circle_colors, pump_1_status_value, pump_2_status_value, pump_3_status_value, pump_4_status_value, pump_5_status_value
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+    
+def update_alarm_tags_all_pumps():
+    pump1_alarm_tags = ['VPUMP_1_B10[2].{}'.format(i) for i in range(16)]
+    pump2_alarm_tags = ['VPUMP_2_B10[2].{}'.format(i) for i in range(16)]
+    pump3_alarm_tags = ['VPUMP_3_B10[2].{}'.format(i) for i in range(16)]
+    pump4_alarm_tags = ['VPUMP_4_B10[2].{}'.format(i) for i in range(16)]
+    pump5_alarm_tags = ['VPUMP_5_B10[2].{}'.format(i) for i in range(16)]
+    
+    # Simulate reading alarm tags for all pumps
+    pump1_alarm_tag_list = read_alarm_tags_all_pumps(pump1_alarm_tags)
+    pump2_alarm_tag_list = read_alarm_tags_all_pumps(pump2_alarm_tags)
+    pump3_alarm_tag_list = read_alarm_tags_all_pumps(pump3_alarm_tags)
+    pump4_alarm_tag_list = read_alarm_tags_all_pumps(pump4_alarm_tags)
+    pump5_alarm_tag_list = read_alarm_tags_all_pumps(pump5_alarm_tags)
+
+    # Simulate determining active alarms for each pump
+    pump1_active_alarms = [tag for tag, value in pump1_alarm_tag_list.items() if value == 1]
+    pump2_active_alarms = [tag for tag, value in pump2_alarm_tag_list.items() if value == 1]
+    pump3_active_alarms = [tag for tag, value in pump3_alarm_tag_list.items() if value == 1]
+    pump4_active_alarms = [tag for tag, value in pump4_alarm_tag_list.items() if value == 1]
+    pump5_active_alarms = [tag for tag, value in pump5_alarm_tag_list.items() if value == 1]
+
+    # Simulate determining alarm status for each pump
+
+    circle_colors, pump_1_status_value, pump_2_status_value, pump_3_status_value, pump_4_status_value, pump_5_status_value = update_circle_color()
+    pump1_alarm_status = pump_1_status_value
+    pump2_alarm_status = pump_2_status_value
+    pump3_alarm_status = pump_3_status_value
+    pump4_alarm_status = pump_4_status_value
+    pump5_alarm_status = pump_5_status_value
+
+    # Construct pump master status dictionary
+    pumpmasterstatus = {
+        'pump1status': {'alarm_status': pump1_alarm_status, 'active_alarms': pump1_active_alarms},
+        'pump2status': {'alarm_status': pump2_alarm_status, 'active_alarms': pump2_active_alarms},
+        'pump3status': {'alarm_status': pump3_alarm_status, 'active_alarms': pump3_active_alarms},
+        'pump4status': {'alarm_status': pump4_alarm_status, 'active_alarms': pump4_active_alarms},
+        'pump5status': {'alarm_status': pump5_alarm_status, 'active_alarms': pump5_active_alarms}
+    }
+    
+    return pumpmasterstatus
+
--- website_1.py
+++ website_1.py
@@ -88,8 +88,7 @@ def page_7_bp():
     return render_template('page_7.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
+
 
 
 '''  
@@ -97,3 +96,127 @@ git config --global user.email "you@example.com"
 git config --global user.name "Your Name
 
 "'''
+
+
+'''
+SCADA Simulation
+'''
+
+
+
+from flask import Flask, render_template, jsonify
+import time
+from Reading import read_plc_tag, update_circle_color, update_alarm_tags_all_pumps
+from pylogix import PLC
+import random
+from Reading import pump_1_status_value, pump_2_status_value, pump_3_status_value, pump_4_status_value, pump_5_status_value
+
+def simulate_plc_data():
+    # Simulate pump vacuum data
+   
+    pump_vacuum_data = [random.randint(0, 100) for _ in range(5)]
+    
+    # Simulate alarm status
+    
+    
+    # Simulate separator pressure data
+    separator_pressure_data = [random.uniform(0, 10) for _ in range(5)]
+    
+    return pump_vacuum_data, separator_pressure_data
+
+# Routes for pump 1
+@app.route('/get_pump1vacuum_data')
+def get_pump1vacuum_data():
+    
+    pump_vacuum_data, _ = simulate_plc_data()
+    return jsonify({'pump1vacuum': pump_vacuum_data[0]})
+
+
+   
+    
+
+@app.route('/get_VACUUM_1_SEPARATOR_PRESSURE_data')
+def get_VACUUM_1_SEPARATOR_PRESSURE_data():
+  
+    _, separator_pressure_data = simulate_plc_data()
+    return jsonify({'seperator1_psi': separator_pressure_data[0]})
+
+# Routes for pump 2
+@app.route('/get_pump2vacuum_data')
+def get_pump2vacuum_data():
+   
+    pump_vacuum_data, _= simulate_plc_data()
+    return jsonify({'pump2vacuum': pump_vacuum_data[1]})
+
+@app.route('/get_pump2vacuum_alarm_status')
+def get_pump2vacuum_alarm_status():
+    if pump_2_status_value == 1:
+        return jsonify({'alarm_status': 'True'})
+    else:
+        return jsonify({'alarm_status': 'False'})
+
+@app.route('/get_VACUUM_2_SEPARATOR_PRESSURE_data')
+def get_VACUUM_2_SEPARATOR_PRESSURE_data():
+   
+    _, separator_pressure_data = simulate_plc_data()
+    return jsonify({'seperator2_psi': separator_pressure_data[1]})
+
+# Routes for pump 3 (similar structure as pump 2)
+
+# Routes for pump 4 (similar structure as pump 2)
+
+# Routes for pump 5 (similar structure as pump 2)
+
+
+'''Utilities'''
+@app.route('/get_circle_color')
+def get_circle_color():
+    circle_colors_tuple = update_circle_color()
+    circle_colors = circle_colors_tuple[0]
+    
+
+@app.route('/check_animation_status')
+def check_animation_status():
+   
+    results = []
+    circle_colors_tuple = update_circle_color()
+    circle_colors = circle_colors_tuple[0]
+    if circle_colors['pump1color'] == 'green':
+        print("updating pump 1 status to green")
+        results.append({'status': 'Animation 1 started'})
+        pump_1_status_value = 0
+    elif circle_colors['pump1color'] != 'green':
+        results.append({'status': 'Animation 1 stopped'})
+        print("updating pump 1 status to red")
+        pump_1_status_value = 1
+    if circle_colors['pump2color'] == 'green':
+        results.append({'status': 'Animation 2 started'})
+        print("updating pump 2 status to green")
+        pump_2_status_value = 0
+    elif circle_colors['pump2color'] != 'green':
+        results.append({'status': 'Animation 2 stopped'})
+        print("updating pump 2 status to red")
+        pump_2_status_value = 1
+    if circle_colors['pump3color'] == 'green':
+        print("updating pump 3 status to green")
+        results.append({'status': 'Animation 3 started'})
+    elif circle_colors['pump3color'] != 'green':
+        results.append({'status': 'Animation 3 stopped'})
+        print("updating pump 3 status to red")
+    if circle_colors['pump4color'] == 'green':
+        results.append({'status': 'Animation 4 started'})
+        print("updating pump 4 status to green")
+    elif circle_colors['pump4color'] != 'green':
+        results.append({'status': 'Animation 4 stopped'})
+        print("updating pump 4 status to red") 
+    if circle_colors['pump5color'] == 'green':
+        results.append({'status': 'Animation 5 started'})
+        print("updating pump 5 status to green")
+    elif circle_colors['pump5color'] != 'green':
+        results.append({'status': 'Animation 5 stopped'})
+        print("updating pump 5 status to red")   
+    return jsonify(results, circle_colors,pump_1_status_value, pump_2_status_value)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)

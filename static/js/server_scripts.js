2d079fca464c847225ccc2e3d351989a9f324f51
Jeffrey Reagan <jeffreyreagan02@gmail.com>
2024-03-18 21:17:32 -0400

    modified:   graph_data.txt 	modified:   static/js/server_scripts.js working chart 1

--- static/js/server_scripts.js
+++ static/js/server_scripts.js
@@ -707,7 +707,8 @@ function renderChart() {
                       font: {size: 25} // Adjust the font size of the chart title
                     },
                     tooltip: {
-                        enabled: true,}
+                        enabled: true,
+                    }
                   }
                 // Chart options (e.g., scales, tooltips, etc.)
                 // See Chart.js documentation for available options
